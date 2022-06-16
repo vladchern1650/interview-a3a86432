f82d36caa3dc9d71d6023baf5a556851ad985357
vchernyshov <vchernyshov@icicall.com>
2022-06-16 17:36:01 +0300

    Initialize project

---
+++ src/main/java/com/vladchern/interview/InterviewA3a86432Application.java
@@ -0,0 +1,13 @@
+package com.vladchern.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewA3a86432Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewA3a86432Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/vladchern/interview/InterviewA3a86432ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladchern.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewA3a86432ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
